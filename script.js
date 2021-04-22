4360c82f21408042e5d20a2841d898e27916c39d
Kalisetti dileep <www.dileep.com999@gmail.com>
2021-04-22 11:51:46 +0530

    start project

---
+++ script.js
@@ -0,0 +1,23 @@
+var menuBtn=document.getElementById("menuBtn");
+var sideNav=document.getElementById("sideNav");
+
+sideNav.style.right="-250px";
+
+function fun(){
+
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+
+    }
+    else{
+        sideNav.style.right="-250px";
+
+    }
+
+}
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed:1000,
+    speedAsDuration: true 
+});
+
